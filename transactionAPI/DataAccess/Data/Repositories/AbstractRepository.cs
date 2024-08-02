86dfd84e09c3312c889f07b93be601fd18ae017b
Vladyslav <vladmelhnik@gmail.com>
2024-08-03 00:16:11 +0300

    implement transaction repository

---
+++ transactionAPI/DataAccess/Data/Repositories/AbstractRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace transactionAPI.DataAccess.Data.Repositories
+{
+    public class AbstractRepository
+    {
+        protected readonly ApplicationDbContext _context;
+
+        protected AbstractRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
--- transactionAPI/DataAccess/Data/Repositories/TransactionRepository.cs
+++ transactionAPI/DataAccess/Data/Repositories/TransactionRepository.cs
@@ -1,23 +1,51 @@
-﻿using System.Transactions;
+﻿using Microsoft.EntityFrameworkCore;
 using transactionAPI.DataAccess.Data.Interfaces;
+using transactionAPI.Entities;
 
 namespace transactionAPI.DataAccess.Data.Repositories
 {
-    public class TransactionRepository : IRepository<Transaction>
+    public class TransactionRepository : AbstractRepository, IRepository<Transaction>
     {
-        public Task AddAsync(Transaction entity)
+        private readonly DbSet<Transaction> dbSet;
+        public TransactionRepository(ApplicationDbContext context) : base(context)
         {
-            throw new NotImplementedException();
+            dbSet = context.Set<Transaction>();
+        }
+        public async Task AddAsync(Transaction entity)
+        {
+            await dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
         }
 
-        public Task<IEnumerable<Transaction>> GetAllAsync()
+        public async Task<IEnumerable<Transaction>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            return await dbSet.ToListAsync();
         }
 
-        public Task<bool> UpdateAsync(Transaction entity)
+        public async Task<bool> UpdateAsync(Transaction entity)
         {
-            throw new NotImplementedException();
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+
+            var result = await dbSet.FindAsync(entity.Id);
+
+            if (result != null)
+            {
+                result.TransactionId = entity.TransactionId;
+                result.Name = entity.Name;
+                result.Email = entity.Email;
+                result.Amount = entity.Amount;
+                result.TransactionDate = entity.TransactionDate;
+                result.ClientLocation = entity.ClientLocation;
+
+                dbSet.Update(result);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            
+            return false;
         }
     }
 }
